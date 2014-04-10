cc68191c923c291f69bf47dc22f062abf45c35f2
TJ Krusinski <tjkrus@gmail.com>
2014-04-10 16:18:17 -0400

    fix formatting

--- routes/index.js
+++ routes/index.js
@@ -5,22 +5,22 @@ var redis = require('../lib/redis');
 var _ = require('underscore');
 
 /**
- *	Save our message 
+ *  Save our message 
  */
 exports.saveMessage = function(req, res, next) {
-	var messages = _.clone(req.body);
-	redis.save(messages, function(err, data){
-		if (err) return res.send(503, err);
+  var messages = _.clone(req.body);
+  redis.save(messages, function(err, data){
+    if (err) return res.send(503, err);
     next();
     redis.trim();
-	});
+  });
 };
 
 /**
- *	Send our message
+ *  Send our message
  */
 exports.sendMessage = function(req, res, next) {
-	var messages = req.body;
+  var messages = req.body;
   messages.forEach(socket.send);
   res.send(200, 'success');
 };
